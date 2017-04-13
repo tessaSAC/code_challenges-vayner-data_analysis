611d7a4469460117afc223579034a2af650acc01
tessaSAC <tessaSAC@users.noreply.github.com>
2017-04-13 18:11:58 -0400

    pauses using neatCsv

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
+const fs = require('fs');
 const neatCsv = require('neat-csv');
-let csv1;
+
 
 // get data from csv1:
-fs.readFile('../files/source1.csv', function (err, data) {
+fs.readFile(`${ __dirname }/../files/source1.csv`, function (err, data) {
+	if (err) console.err(`Warning: ${ err }`);
+
 	// Returns a promise for an array with the parsed CSV.
 	neatCsv(data).then(data => {
-	    csv1 = data;
-	});
+	    uniqsPerMonth('02', data);
+	})
+	.catch(err);
 });
 
 // Sample output:
@@ -41,6 +45,41 @@ fs.readFile('../files/source1.csv', function (err, data) {
 // CAMPAIGN: initiative_audience_asset
 
 // 1. unique campaigns in Feb -- csv1 parse date
+
+function uniqsPerMonth(month, data) {
+	// month = month separated
+	// const month = date.split('-')[1]; -- change `date` to `month`
+	const monthRegExp = new RegExp(`\d{4}-${month}-\d{2}`);
+
+	// trim any leading zeros -- nvm; will just create more work in comparison stage
+	// month = month.replace(/^0+/g, '');
+
+	const campaigns = new Set();
+
+	// For each campaign:
+	for (Row in data) {
+
+		// If Row matches the date
+		if (Row.date.match(month)) {
+			console.log(Row.date);
+			// // If campaign doesn't exist add campaign
+			// if (!campaigns.has(Row.campaign)) {
+			// 	campaigns.add({
+			// 		Row.campaign: Row
+			// 	});
+			// }
+
+			// // Else combine with previous campaign
+			// else {
+			// 	campaigns[Row.campaign] = Object.assign({}, campaigns[Row.campaign], Row);
+			// }
+		}
+	}
+
+	// return campaigns;
+}
+
+
 // 2. total conversions PLANT initiative
 	// Add all conversions of type x || y that contains PLANTS initiative
 // 3. audience + asset's lowest CPM
